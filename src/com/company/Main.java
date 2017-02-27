857e0bd183f7e1a1200a8d36a904792453a2dd74
DomiCore <kern.dominic@web.de>
2017-02-27 16:42:26 +0100

    Funktioniert alles Fehlt noch direkt steuerung

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,13 +5,21 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Main {
-    private static byte speed, steering;
+    private static byte speed, steering = 50;
 
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
+        System.out.print("Start");
+        SendDataThread sendDataThread = new SendDataThread();
+        sendDataThread.start();
+
+        ReceiveDataThread receiveDataThread = new ReceiveDataThread(sendDataThread);
+        receiveDataThread.start();
+    }
+
+    /*
+            Scanner scanner = new Scanner(in);
 
         ServerSocket serverSocket = new ServerSocket(4572);
 
@@ -19,7 +27,7 @@ public class Main {
         InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
 
-        Thread t = new SendThread(outputStream, inputStream);
+        Thread t = new ReceiveDataThread(outputStream, inputStream);
         t.start();
 
         byte[] buffer = new byte[8];
@@ -40,31 +48,98 @@ public class Main {
         } else {
             System.out.println("FAILURE");
         }
-    }
+     */
 
-    static class SendThread extends Thread {
-        OutputStream outputStream;
-        InputStream inputStream;
+    static class ReceiveDataThread extends Thread {
+        Thread otherDependenThread;
 
-        public SendThread(OutputStream outputStream, InputStream inputStream) {
-            this.outputStream = outputStream;
-            this.inputStream = inputStream;
+        ReceiveDataThread(Thread otherDependenThread) {
+            this.otherDependenThread = otherDependenThread;
         }
 
+        //receive from app
         @Override
         public void run() {
-            byte [] buffer = new byte[50];
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(4586);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
 
             while (true) {
                 try {
-                    outputStream.write(new byte[]{speed, steering});
-                    inputStream.read(buffer);
-                    sleep(200);
+                    Socket socket = serverSocket.accept();
+                    System.out.print("Server OK");
+                    InputStream in = socket.getInputStream();
+                    OutputStream out = socket.getOutputStream();
+
+                    byte[] inputData = new byte[2];
+                    byte[] serverOk = "SERVER OK".getBytes();
+
+
+                    while (true) {
+                        in.read(inputData);
+                        speed = inputData[0];
+                        steering = inputData[1];
+
+                        System.out.println("speed" + speed);
+                        System.out.println("steering" + steering);
+                        otherDependenThread.interrupt();
+
+                        out.write(serverOk);
+                    }
                 } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    System.out.println("Reconnecting to App");
                 }
+            }
+        }
+    }
+
+    //send to Pi
+    static class SendDataThread extends Thread {
+        @Override
+        public void run() {
+            ServerSocket serverSocket = null;
+            try {
+                serverSocket = new ServerSocket(4587);
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+            while (true) {
+                try {
+                    Socket socket = serverSocket.accept();
+                    InputStream in = socket.getInputStream();
+                    OutputStream out = socket.getOutputStream();
+
+                    byte[] sendData = new byte[2];
+                    byte[] buffer = new byte[20];
+
+                    while (true) {
+
+                        sendData[0] = speed;
+                        sendData[1] = steering;
+                        out.write(sendData);
+                        System.out.println("Sende an Pi Speed:" + sendData[0] + "steering" + sendData[1]);
+                        in.read(buffer);
+
+                        if (!interrupted()) {
+                            sleep(1000);
+                        }
+                    }
+                } catch (IOException e) {
+                    //e.printStackTrace();
+                    System.out.println("Reconnecting to Pi");
+                }
+            }
+        }
+
+        void sleep(int time) {
+            try {
+                Thread.sleep(time);
+            } catch (InterruptedException ignore) {
             }
         }
     }
